322bac24a5ab9173aa8d168d339bf337984f8690
mmbbhh <mabinheng@gmail.com>
2020-02-14 19:46:37 +0800

    静态资源访问和优先级

---
+++ day02/src/main/java/com/day01/day01/Day01Application.java
@@ -0,0 +1,15 @@
+package com.day01.day01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day01Application.class, args);
+	}
+
+}
+
+
